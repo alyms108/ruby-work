0b3416614eb005eb1f8a039e086273241d3542d1
alyms108 <amspratt89@gmail.com>
2015-06-23 17:49:56 +0000

    Finished up to 'Booleans'

---
+++ practice.rb
@@ -0,0 +1,26 @@
+# Numbers & Strings
+#puts 2 + 2
+#puts 3 / 4
+#puts 3.0/4
+#puts Math.sqrt(4)
+#puts "Whatevs"
+#puts "I'm missing" + "an important space!"
+
+# Variables & Interpolation
+# name1 = "Lavy"
+# puts name1
+# name2 = "Didra"
+# puts name2
+# name3 = name1
+# puts name3
+# puts "Hello, my name is #{name1}"
+
+# Booleans
+# puts 1 == 1
+# puts 2 > 5
+# puts 5 != 5
+# bool1 = true
+# bool2 = false
+# puts bool1 && bool2
+# puts bool1 || bool2
+# puts !bool2
